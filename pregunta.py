fbd829ded85fcc1f165f2e4b2d411ba33dc1f6a1
Valopezmo88 <89362841+Valopezmo88@users.noreply.github.com>
2023-10-18 14:13:59 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -12,9 +12,22 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df.reset_index(inplace=True,drop=True)
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+    
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
+    
+    df.dropna(axis='index',inplace=True)
+    df.drop_duplicates(inplace=True)
 
-    #
-    # Inserte su código aquí
-    #
+    df = df.apply(lambda x: x.str.lower() if x.dtype == 'object' else x)
+     
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
+    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
+    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
 
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)   
+    
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis='index',inplace=True)
     return df
