681a629c46fb1055f1a376704cb8ce3a0a1a5931
Valopezmo88 <89362841+Valopezmo88@users.noreply.github.com>
2023-10-27 11:04:22 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -8,19 +8,17 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
+
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
-    
-    df.reset_index(inplace=True,drop=True)
+
     df.dropna(inplace=True)
-    
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
-    df = df.apply(lambda x: x.str.lower() if x.dtype == 'object' else x)
-    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
-    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)   
+
+    df = df.apply(lambda x: x.astype(str).str.replace("_","-").str.replace("-"," ").str.replace("$","").str.replace(",",""))
+    df = df.apply(lambda x: x.astype(str).str.lower().str.strip(" "))
+    df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"],format='mixed', dayfirst = True)
+    df.monto_del_credito = df.monto_del_credito.astype(float)
 
     df.drop_duplicates(inplace=True)
 
